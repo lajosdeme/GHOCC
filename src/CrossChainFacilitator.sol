e20050c68abe9fe6270ff3a46dadcd9b42e1674d
lajosdeme <44027725+lajosdeme@users.noreply.github.com>
2024-01-19 18:56:14 +0100

    set up basic functions

--- src/CrossChainFacilitator.sol
+++ src/CrossChainFacilitator.sol
@@ -8,6 +8,8 @@ import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications
 import {IERC20} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.0/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.0/contracts/token/ERC20/utils/SafeERC20.sol";
 import {EnumerableMap} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.0/contracts/utils/structs/EnumerableMap.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./gho/interfaces/IGhoToken.sol";
 
 /* 
 - take USDC as payment, mint GHO in return
@@ -16,7 +18,7 @@ import {EnumerableMap} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppe
 - you can redeem USDC with GHO
  */
 
- /* 
+/* 
  steps:
  1. USDC, GHO, Aave governance hardcoded
  2. Have a capacitiy and a limit
@@ -24,8 +26,63 @@ import {EnumerableMap} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppe
  4. have another function for sending cross chain
   */
 
-
-
 contract CrossChainFacilitator {
+    // GHO token address
+    IGhoToken public immutable GHO_TOKEN;
 
+    // USDC token address
+    IERC20 public immutable USDC_TOKEN;
+
+    uint256 public constant MAX_FEE = 1e4;
+
+    // The GHO treasury, the recipient of fee distributions
+    address private _ghoTreasury;
+
+    // The facilitator fee, expressed in bps (a value of 10000 results in 100.00%)
+    uint256 private _fee;
+
+    address private _aaveGovernance;
+
+    constructor(address ghoToken, address ghoTreasury, address aaveGovernance, uint256 fee) {
+        require(fee <= MAX_FEE, 'CrossChainFacilitator: Fee out of range');
+        GHO_TOKEN = IGhoToken(ghoToken);
+        _updateGhoTreasury(ghoTreasury);
+        _updateFee(fee);
+        _updateAaveGovernance(newAaveGovernance);
+    }
+
+    function updateFee(uint256 newFee) external { // TODO
+        _updateFee(newFee);
+    }
+
+    function updateGhoTreasury(address newGhoTreasury) external { // TODO
+        _updateGhoTreasury(newGhoTreasury);
+    }
+
+    function getFee() external view override returns (uint256) {
+        return _fee;
+    }
+
+    function getGhoTreasury() external view override returns (address) {
+        return _ghoTreasury;
+    }
+
+    function _updateFee(uint256 newFee) internal {
+        require(newFee <= MAX_FEE, 'CrossChainFacilitator: Fee out of range');
+        uint256 oldFee = _fee;
+        _fee = newFee;
+        emit FeeUpdated(oldFee, newFee);
+    }
+
+    function _updateGhoTreasury(address newGhoTreasury) internal {
+        address oldGhoTreasury = _ghoTreasury;
+        _ghoTreasury = newGhoTreasury;
+        emit GhoTreasuryUpdated(oldGhoTreasury, newGhoTreasury);
+    }
+
+    function _updateAaveGovernance(address newAaveGovernance) internal {
+        address oldAaveGovernance = _aaveGovernance;
+        _aaveGovernance = newAaveGovernance;
+        emit AaveGovernanceUpdated(oldAaveGovernance, newAaveGovernance);
+    }
 }
--- src/interfaces/ICrossChainFacilitator.sol
+++ src/interfaces/ICrossChainFacilitator.sol
@@ -1,6 +1,41 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-interface ICrossChainFacilitator {
-    
+import "../gho/interfaces/IGhoFacilitator.sol";
+import "../gho/interfaces/IGhoToken.sol";
+
+interface ICrossChainFacilitator is IGhoFacilitator {
+    /**
+     * @dev Emitted when the percentage fee is updated
+     * @param oldFee The old fee (in bps)
+     * @param newFee The new fee (in bps)
+     */
+    event FeeUpdated(uint256 oldFee, uint256 newFee);
+
+    event AaveGovernanceUpdated(address oldAaveGovernance, address newAaveGovernance);
+
+    /**
+     * @notice Returns the address of the GHO token contract
+     * @return The address of the GhoToken
+     */
+    function GHO_TOKEN() external view returns (IGhoToken);
+
+    /**
+     * @notice Returns the maximum value the fee can be set to
+     * @return The maximum percentage fee of the minted amount that the fee can be set to (in bps).
+     */
+    function MAX_FEE() external view returns (uint256);
+
+    /**
+     * @notice Updates the percentage fee. It is the percentage of the minted amount that needs to be repaid.
+     * @dev The fee is expressed in bps. A value of 100, results in 1.00%
+     * @param newFee The new percentage fee (in bps)
+     */
+    function updateFee(uint256 newFee) external;
+
+    /**
+     * @notice Returns the percentage of each mint taken as a fee
+     * @return The percentage fee of the minted amount that needs to be repaid, on top of the principal (in bps).
+     */
+    function getFee() external view returns (uint256);
 }
