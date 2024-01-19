aee4d75236ca8d763595c57bbeaff0acd5382666
lajosdeme <44027725+lajosdeme@users.noreply.github.com>
2024-01-19 18:33:40 +0100

    set up external dependencies

--- src/CrossChainFacilitator.sol
+++ src/CrossChainFacilitator.sol
@@ -1,6 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
+import {OwnerIsCreator} from "@chainlink/contracts-ccip/src/v0.8/shared/access/OwnerIsCreator.sol";
+import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
+import {IERC20} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.0/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.0/contracts/token/ERC20/utils/SafeERC20.sol";
+import {EnumerableMap} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.0/contracts/utils/structs/EnumerableMap.sol";
 
 /* 
 - take USDC as payment, mint GHO in return
@@ -16,6 +23,8 @@ pragma solidity ^0.8.17;
  3. when received UDC mint equivalent of GHO
  4. have another function for sending cross chain
   */
+
+
 
 contract CrossChainFacilitator {
 
